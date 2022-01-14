ca10b0753ed4e5b7748ea022a0af2358a98d3244
Noe Osorio <osorionoe@gmail.com>
2022-01-13 19:03:39 -0600

    Responsive design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   const [reminders, setReminders] = useState([
@@ -22,17 +23,29 @@ function App() {
   };
 
   return (
-    <div>
-      <header>
-        <h1>Reminders</h1>
+    <div className="container">
+      <header className="header">
+        <h1 className="header-title">Reminders</h1>
       </header>
-      <main>
-        <ul>
+      <main className="reminders">
+        <ul className="reminders-list">
           {reminders.map((reminder, index) => (
             <ReminderUI key={`Reminders-${index}`} reminder={reminder} />
           ))}
-          <li>
-            <button onClick={add}>Add new reminder</button>
+          <li className="list-element">
+            <button className="reminders-add" onClick={add}>
+              +
+            </button>
+          </li>
+        </ul>
+        <ul className="reminders-list">
+          {reminders.map((reminder, index) => (
+            <ReminderUI key={`Reminders-${index}`} reminder={reminder} />
+          ))}
+          <li className="list-element">
+            <button className="reminders-add" onClick={add}>
+              +
+            </button>
           </li>
         </ul>
       </main>
@@ -44,7 +57,7 @@ export default App;
 
 function ReminderUI({ reminder, completed }) {
   return (
-    <li>
+    <li className="list-element">
       <p>{reminder}</p>
       {completed ? <span>Completado</span> : <></>}
     </li>
